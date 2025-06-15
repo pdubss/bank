0ca4634d1748ec3278bf03bc3c53e56266688631
Perry <perryy41996@yahoo.com>
2025-06-15 05:21:10 -0400

    adjusted heights of dashboard components

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -254,7 +254,7 @@ export default function Dashboard() {
       {userLoading ? (
         <Spinner />
       ) : (
-        <div className="flex max-h-[4/5] w-[22rem] flex-col gap-10 border p-6 text-center">
+        <div className="flex h-[4/5] w-[22rem] flex-col gap-10 border p-6 text-center">
           <h2 className="border-b text-2xl font-bold">Account Details</h2>
           <h3 className="font-semibold">Account Number</h3>
           <p>{account.accountNumber}</p>
@@ -270,7 +270,7 @@ export default function Dashboard() {
           ) : null}
         </div>
       )}
-      <div className="flex max-h-[4/5] w-[22rem] flex-col gap-20 border p-6 md:w-[30rem]">
+      <div className="flex h-[4/5] w-[22rem] flex-col gap-20 border p-6 md:w-[30rem]">
         <div>
           <h2 className="border-b text-center text-2xl font-bold">
             Action Center
